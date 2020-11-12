f61f27a38ccee4dafd3e5b13e8ebbe0c95843678
maharajabhai <69481703+maharajabhai@users.noreply.github.com>
2020-11-12 17:39:51 -0500

    Create Untitled.m

---
+++ Untitled.m
@@ -0,0 +1,8 @@
+clc
+clear
+
+fprintf('\n************************************************\n')
+fprintf('*  Name:  Bibhus Luitel   Date:  11/10/20     *\n')
+fprintf('*  Seat/Table:  G    File: Class_20_App.m     *\n')
+fprintf('*  Instructor:  Ben Grier 05:20                      *\n')                       
+fprintf('************************************************\n')
