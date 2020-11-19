8892fbe59992feb2aaada96ba24b2e8fe3a421b9
maharajabhai <69481703+maharajabhai@users.noreply.github.com>
2020-11-19 17:40:59 -0500

    Update SDP_Hangman.m

--- SDP_Hangman.m
+++ SDP_Hangman.m
@@ -6,71 +6,6 @@ fprintf('*  Name:  Bibhus Luitel, Billy Dhillon, Husam Ahmed, Carter Hornish   D
 fprintf('*  Seat/Table:  G   File: Class_20_App.m     *\n')
 fprintf('*  Instructor:  Ben Grier 05:20                      *\n')                       
 fprintf('************************************************\n')
-%% Create Beginning Display
-fprintf('Welcome to Hangman!\n')
-fprintf(' _______')
-fprintf('|       |')
-fprintf('|')
-fprintf('|')
-fprintf('|')
-fprintf('----')
-fprintf('You will have 6 tries to correctly guess the word!\n')
-fprintf('As a hint this word falls under the category of animals. Good Luck!')
-%% Create word Bank
-word_bank={'cat', 'dog', 'horse', 'zebra', 'donkey' ,'monkey', 'rabbit' ,'squirrel', 'goat'};
-word = randsample(word_bank, 1);
-%% Create Hangman Display for Wrong answers
-%One Wrong guess 
-display("You have used your first guess!");
-display(' _______');
-display('|       |');
-display('|       0');
-display('|');
-display('|');
-display('----');
-%Second Wrong guess
-display('You have used your second guess!’');
-display(' _______');
-display('|       |');
-display('|       0');
-display('|       |');
-display('|');
-display('----');
-%Third Wrong guess
-display('You have used your third guess!’');
-display(' _______');
-display('|       |');
-display('|       0');
-display('|      \|');
-display('|');
-display('----');
-%Fourth Wrong guess
-display('You have used your fourth guess!’');
-display(' _______');
-display('|       |');
-display('|       0');
-display('|      \|/');
-display('|');
-display('----');
-%Fifth Wrong guess
-display('You have used your fifth guess!');
-display(' _______');
-display('|       |');
-display('|       0');
-display('|      \|/');
-display('|       | ');
-display('|      / ');
-display('----');
-
-%Sixth Wrong guess 
-display('You have used your last guess! Game over!');
-display(' _______');
-display('|       |');
-display('|       0');
-display('|      \|/');
-display('|       | ');
-display('|      / \');
-display('----');
 
 %% 
 fprintf('Welcome to Hangman!\n')
@@ -87,7 +22,7 @@ word = randsample(word_bank, 1);
 arrayOfDashes = outputDashes(word)
 numOfGuesses = 0;	
 while (numOfGuesses < 6)
-	x = input("Please enter your guess: ");
+	x = input("Please enter your guess: ", 's');
 	guessedLetters = storeGuessedLetters(x);
            
 	correspondingDisplay(x, word, arrayOfDashes, numOfGuesses);
